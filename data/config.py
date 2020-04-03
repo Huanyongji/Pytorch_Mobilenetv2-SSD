873aefc101c130ab1d559bc0d19742682a80af4f
jihuanyong <huanyongji@gmail.com>
2020-04-03 11:39:13 +0800

    Update config.py
    Add mv2-ssd config

--- data/config.py
+++ data/config.py
@@ -11,20 +11,37 @@ COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
 MEANS = (104, 117, 123)
 
 # SSD300 CONFIGS
+# voc = {
+#     'num_classes': 21,
+#     'lr_steps': (80000, 100000, 120000),
+#     'max_iter': 120000,
+#     'feature_maps': [38, 19, 10, 5, 3, 1],
+#     'min_dim': 300,
+#     'steps': [8, 16, 32, 64, 100, 300],
+#     'min_sizes': [30, 60, 111, 162, 213, 264],
+#     'max_sizes': [60, 111, 162, 213, 264, 315],
+#     'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+#     'variance': [0.1, 0.2],
+#     'clip': True,
+#     'name': 'VOC',
+# }
+
+# MV2-SSD CONFIGS
 voc = {
     'num_classes': 21,
     'lr_steps': (80000, 100000, 120000),
     'max_iter': 120000,
-    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'feature_maps': [19, 10, 5, 3, 2, 1],
     'min_dim': 300,
-    'steps': [8, 16, 32, 64, 100, 300],
+    'steps': [16, 30, 60, 100, 150, 300],
     'min_sizes': [30, 60, 111, 162, 213, 264],
     'max_sizes': [60, 111, 162, 213, 264, 315],
-    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3]],
     'variance': [0.1, 0.2],
-    'clip': True,
+    'clip': False,
     'name': 'VOC',
 }
+
 
 coco = {
     'num_classes': 201,
